a4a015c3b9e6165cceda6ffd98d1d6dd5341413c
Matt Wynne <matt@mattwynne.net>
2011-04-18 12:54:41 +0100

    Show errors in page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,7 @@ Errbit::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
